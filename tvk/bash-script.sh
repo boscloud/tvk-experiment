5c7c6f945b92962b507bc82f91a7d1e28b397de5
Andrew Dawson <adawson@netafy.com>
2021-02-10 09:06:27 -0500

    Updates

---
+++ tvk/bash-script.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -ex
+
+kubectl apply -f /backupplan/mysql-sample-backupplan.yaml
+
+sleep 10
+
+kubectl delete -f /backup/mysql-sample-backup.yaml || :
+
+kubectl apply -f /backup/mysql-sample-backup.yaml
+
+counter=0
+
+while [ $counter -lt 300 ]; do
+  if [ "$(kubectl get backup mysql-label-backup -o go-template='{{ .status.phaseStatus }}')" == "Completed" ]; then
+    echo "backup completed"
+    exit 0
+  fi
+  let counter=counter+1
+  sleep 5
+done
+
+if [ $counter -ge 300 ]; then
+  echo "backup failed"
+  exit 1
+fi
